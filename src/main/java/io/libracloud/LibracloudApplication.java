34e39e2d77ec5993806ba6ff590b1aa004733c70
Ketan R <ketan_r598@outlook.com>
2024-09-20 00:03:03 +0530

    initial-commit

---
+++ src/main/java/io/libracloud/LibracloudApplication.java
@@ -0,0 +1,13 @@
+package io.libracloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibracloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibracloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/libracloud/LibracloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.libracloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibracloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
